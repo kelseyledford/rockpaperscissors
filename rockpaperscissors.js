fd6acb048d5fd35c250a275cfc746359d1d443e2
Kelsey Ledford <kelsey.d.ledford@gmail.com>
2014-04-11 16:40:52 -0700

    Add image files, change prompt to happen on click instead of on loading the page, allow player to play again without refreshing page, adjust style

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,85 +1,93 @@
 //"You win/lose" notification, preferably under the logo
 //If result is a tie, repeat the game
 //Play again? choice
-//on click 
 //2+ user game
+//Fix image so that it is centered
 
-var userChoice = prompt("Do you choose rock, paper, scissors, lizard or Spock?");
-var userChoice = userChoice.toLowerCase();
 
-var computerChoice = Math.random();
-if (computerChoice >= 0, computerChoice <= 0.2) {
-	computerChoice="rock";
-} else if (computerChoice >= 0.21, computerChoice <= 0.4) {
-	computerChoice="paper";
-} else if (computerChoice >= 0.41, computerChoice <= 0.6) {
-	computerChoice="scissors";
-} else if (computerChoice >= 0.61, computerChoice <= 0.8) {
-	computerChoice="lizard";
-} else {
-	computerChoice="spock";
+
+window.onload = function(){
+	var userChoice;
+
+	var logo = document.getElementById("logo");
+	logo.onclick = function clickToChoose() {
+		userChoice = prompt("Do you choose rock, paper, scissors, lizard or Spock?");
+		userChoice = userChoice.toLowerCase();
+		compare(userChoice, computerChoice);
+	};
+
+	var computerChoice = Math.random();
+	if (computerChoice >= 0, computerChoice <= 0.2) {
+		computerChoice="rock";
+	} else if (computerChoice >= 0.21, computerChoice <= 0.4) {
+		computerChoice="paper";
+	} else if (computerChoice >= 0.41, computerChoice <= 0.6) {
+		computerChoice="scissors";
+	} else if (computerChoice >= 0.61, computerChoice <= 0.8) {
+		computerChoice="lizard";
+	} else {
+		computerChoice="spock";
+	}
+
+	console.log(computerChoice)
+
+	var compare = function (choice1, choice2) {
+		if (choice1 === "rock")
+			if (choice2 === "paper")
+				{document.getElementById("notify").innerHTML="Paper covers rock";}
+			else if (choice2 === "scissors" || choice2 === "sissors" || choice2 === "sisors")
+				{document.getElementById("notify").innerHTML="Rock crushes scissors";}
+			else if (choice2 === "lizard" || choice2 === "lisard" || choice2 === "lizzard")
+				{document.getElementById("notify").innerHTML="Rock crushes lizard";}
+			else if (choice2 === "spock" || choice1 === "spoc" || choice1 === "spok")
+				{document.getElementById("notify").innerHTML="Spock vaporizes rock";}
+			else
+				{document.getElementById("notify").innerHTML="Tie!";}
+		else if (choice1 === "paper")
+			if (choice2 === "rock")
+				{document.getElementById("notify").innerHTML="Paper covers rock";}
+			else if (choice2 === "scissors" || choice2 === "sissors" || choice2 === "sisors")
+				{document.getElementById("notify").innerHTML="Scissors cuts paper";}
+			else if (choice2 === "lizard" || choice2 === "lisard" || choice2 === "lizzard")
+				{document.getElementById("notify").innerHTML="Lizard eats paper";}
+			else if (choice2 === "spock" || choice1 === "spoc" || choice1 === "spok")
+				{document.getElementById("notify").innerHTML="Paper disproves Spock";}
+			else
+				{document.getElementById("notify").innerHTML="Tie!";}
+		else if (choice1 ==="scissors" || choice2 === "sissors" || choice2 === "sisors")
+			if (choice2 === "paper")
+				{document.getElementById("notify").innerHTML="Scissors cuts paper";}
+			else if (choice2 === "rock")
+				{document.getElementById("notify").innerHTML="Rock crushes scissors";}
+			else if (choice2 === "lizard" || choice2 === "lisard" || choice2 === "lizzard")
+				{document.getElementById("notify").innerHTML="Scissors decapitate lizard";}
+			else if (choice2 === "spock" || choice1 === "spoc" || choice1 === "spok")
+				{document.getElementById("notify").innerHTML="Spock smashes scissors";}
+			else
+				{document.getElementById("notify").innerHTML="Tie!";}
+		else if (choice1 ==="lizard" || choice2 === "lisard" || choice2 === "lizzard")
+			if (choice2 === "paper")
+				{document.getElementById("notify").innerHTML="Lizard eats paper";}
+			else if (choice2 === "rock")
+				{document.getElementById("notify").innerHTML="Rock crushes lizard";}
+			else if (choice2 === "scissors" || choice2 === "sissors" || choice2 === "sisors")
+				{document.getElementById("notify").innerHTML="Scissors decapitate lizard";}
+			else if (choice2 === "spock" || choice1 === "spoc" || choice1 === "spok")
+				{document.getElementById("notify").innerHTML="Lizard poisons Spock";}
+			else
+				{document.getElementById("notify").innerHTML="Tie!";}
+		else if (choice1 === "spock" || choice1 === "spoc" || choice1 === "spok")
+			if (choice2 === "paper")
+				{document.getElementById("notify").innerHTML="Paper disproves Spock";}
+			else if (choice2 === "rock")
+				{document.getElementById("notify").innerHTML="Spock vaporizes rock";}
+			else if (choice2 === "lizard" || choice2 === "lisard" || choice2 === "lizzard")
+				{document.getElementById("notify").innerHTML="Lizard poisons Spock";}
+			else if (choice2 === "scissors" || choice2 === "sissors" || choice2 === "sisors")
+				{document.getElementById("notify").innerHTML="Spock smashes scissors";}
+			else
+				{document.getElementById("notify").innerHTML="Tie!";}
+		else 
+			{document.getElementById("notify").innerHTML="Sorry, I didn't understand you.";}
+	};
 }
-
-console.log(computerChoice)
-
-var compare = function (choice1, choice2) {
-	if (choice1 === "rock")
-		if (choice2 === "paper")
-			{document.write ("Paper covers rock");}
-		else if (choice2 === "scissors" || choice2 === "sissors" || choice2 === "sisors")
-			{document.write ("Rock crushes scissors");}
-		else if (choice2 === "lizard" || choice2 === "lisard" || choice2 === "lizzard")
-			{document.write ("Rock crushes lizard");}
-		else if (choice2 === "spock" || choice1 === "spoc" || choice1 === "spok")
-			{document.write ("Spock vaporizes rock");}
-		else
-			{document.write ("Tie!");}
-	else if (choice1 === "paper")
-		if (choice2 === "rock")
-			{document.write ("Paper covers rock");}
-		else if (choice2 === "scissors" || choice2 === "sissors" || choice2 === "sisors")
-			{document.write ("Scissors cuts paper");}
-		else if (choice2 === "lizard" || choice2 === "lisard" || choice2 === "lizzard")
-			{document.write ("Lizard eats paper");}
-		else if (choice2 === "spock" || choice1 === "spoc" || choice1 === "spok")
-			{document.write ("Paper disproves Spock");}
-		else
-			{document.write ("Tie!");}
-	else if (choice1 ==="scissors" || choice2 === "sissors" || choice2 === "sisors")
-		if (choice2 === "paper")
-			{document.write ("Scissors cuts paper");}
-		else if (choice2 === "rock")
-			{document.write ("Rock crushes scissors");}
-		else if (choice2 === "lizard" || choice2 === "lisard" || choice2 === "lizzard")
-			{document.write ("Scissors decapitate lizard");}
-		else if (choice2 === "spock" || choice1 === "spoc" || choice1 === "spok")
-			{document.write ("Spock smashes scissors");}
-		else
-			{document.write ("Tie!");}
-	else if (choice1 ==="lizard" || choice2 === "lisard" || choice2 === "lizzard")
-		if (choice2 === "paper")
-			{document.write ("Lizard eats paper");}
-		else if (choice2 === "rock")
-			{document.write ("Rock crushes lizard");}
-		else if (choice2 === "scissors" || choice2 === "sissors" || choice2 === "sisors")
-			{document.write ("Scissors decapitate lizard");}
-		else if (choice2 === "spock" || choice1 === "spoc" || choice1 === "spok")
-			{document.write ("Lizard poisons Spock");}
-		else
-			{document.write ("Tie!");}
-	else if (choice1 === "spock" || choice1 === "spoc" || choice1 === "spok")
-		if (choice2 === "paper")
-			{document.write ("Paper disproves Spock");}
-		else if (choice2 === "rock")
-			{document.write ("Spock vaporizes rock");}
-		else if (choice2 === "lizard" || choice2 === "lisard" || choice2 === "lizzard")
-			{document.write ("Lizard poisons Spock");}
-		else if (choice2 === "scissors" || choice2 === "sissors" || choice2 === "sisors")
-			{document.write ("Spock smashes scissors");}
-		else
-			{document.write ("Tie!");}
-	else 
-		{document.write ("Sorry, I didn't understand you.")}
-};
-
-compare(userChoice, computerChoice)
