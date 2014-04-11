aa81807e97b22233c88a124e419e92ec32cbc306
Kelsey Ledford <kelsey.d.ledford@gmail.com>
2014-04-11 12:15:01 -0700

    Account for capitalization, common spelling errors, and user choice not being recognized

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,11 +1,12 @@
-//Account for spelling
-//Account for capitalization
-//Account for people choosing something that isn't from the list
-//If result is a tie, repeat the game
 //"You win/lose" notification, preferably under the logo
-//Play again
+//If result is a tie, repeat the game
+//Play again? choice
+//on click 
+//2+ user game
 
 var userChoice = prompt("Do you choose rock, paper, scissors, lizard or Spock?");
+var userChoice = userChoice.toLowerCase();
+
 var computerChoice = Math.random();
 if (computerChoice >= 0, computerChoice <= 0.2) {
 	computerChoice="rock";
@@ -25,58 +26,60 @@ var compare = function (choice1, choice2) {
 	if (choice1 === "rock")
 		if (choice2 === "paper")
 			{document.write ("Paper covers rock");}
-		else if (choice2 === "scissors")
+		else if (choice2 === "scissors" || choice2 === "sissors" || choice2 === "sisors")
 			{document.write ("Rock crushes scissors");}
-		else if (choice2 === "lizard")
+		else if (choice2 === "lizard" || choice2 === "lisard" || choice2 === "lizzard")
 			{document.write ("Rock crushes lizard");}
-		else if (choice2 === "spock")
+		else if (choice2 === "spock" || choice1 === "spoc" || choice1 === "spok")
 			{document.write ("Spock vaporizes rock");}
 		else
 			{document.write ("Tie!");}
-	if (choice1 === "paper")
+	else if (choice1 === "paper")
 		if (choice2 === "rock")
 			{document.write ("Paper covers rock");}
-		else if (choice2 === "scissors")
+		else if (choice2 === "scissors" || choice2 === "sissors" || choice2 === "sisors")
 			{document.write ("Scissors cuts paper");}
-		else if (choice2 === "lizard")
+		else if (choice2 === "lizard" || choice2 === "lisard" || choice2 === "lizzard")
 			{document.write ("Lizard eats paper");}
-		else if (choice2 === "spock")
+		else if (choice2 === "spock" || choice1 === "spoc" || choice1 === "spok")
 			{document.write ("Paper disproves Spock");}
 		else
 			{document.write ("Tie!");}
-	if (choice1 ==="scissors")
+	else if (choice1 ==="scissors" || choice2 === "sissors" || choice2 === "sisors")
 		if (choice2 === "paper")
 			{document.write ("Scissors cuts paper");}
 		else if (choice2 === "rock")
 			{document.write ("Rock crushes scissors");}
-		else if (choice2 === "lizard")
+		else if (choice2 === "lizard" || choice2 === "lisard" || choice2 === "lizzard")
 			{document.write ("Scissors decapitate lizard");}
-		else if (choice2 === "spock")
+		else if (choice2 === "spock" || choice1 === "spoc" || choice1 === "spok")
 			{document.write ("Spock smashes scissors");}
 		else
 			{document.write ("Tie!");}
-	if (choice1 ==="lizard")
+	else if (choice1 ==="lizard" || choice2 === "lisard" || choice2 === "lizzard")
 		if (choice2 === "paper")
 			{document.write ("Lizard eats paper");}
 		else if (choice2 === "rock")
 			{document.write ("Rock crushes lizard");}
-		else if (choice2 === "scissors")
+		else if (choice2 === "scissors" || choice2 === "sissors" || choice2 === "sisors")
 			{document.write ("Scissors decapitate lizard");}
-		else if (choice2 === "spock")
+		else if (choice2 === "spock" || choice1 === "spoc" || choice1 === "spok")
 			{document.write ("Lizard poisons Spock");}
 		else
 			{document.write ("Tie!");}
-	if (choice1 ==="spock")
+	else if (choice1 === "spock" || choice1 === "spoc" || choice1 === "spok")
 		if (choice2 === "paper")
 			{document.write ("Paper disproves Spock");}
 		else if (choice2 === "rock")
 			{document.write ("Spock vaporizes rock");}
-		else if (choice2 === "lizard")
+		else if (choice2 === "lizard" || choice2 === "lisard" || choice2 === "lizzard")
 			{document.write ("Lizard poisons Spock");}
-		else if (choice2 === "spock")
+		else if (choice2 === "scissors" || choice2 === "sissors" || choice2 === "sisors")
 			{document.write ("Spock smashes scissors");}
 		else
 			{document.write ("Tie!");}
+	else 
+		{document.write ("Sorry, I didn't understand you.")}
 };
 
 compare(userChoice, computerChoice)
