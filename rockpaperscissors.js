659fb7adca473c3407da72ddc4b42a58ae6a49ea
Kelsey Ledford <kelsey.d.ledford@gmail.com>
2014-04-12 19:43:27 -0700

    Add a score keeper, add a win/lose notification

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,11 +2,14 @@
 //2+ user game
 //Get rid of little white line after text
 //Change so that userChoice is determined by which of the pictures/divs the user chooses
+//Account for letter switching misspellings
 
 
 window.onload = function(){
 	var userChoice;
 	var computerChoice; 
+	var userCount = 0;
+	var compCount = 0;
 
 	var logo = document.getElementById("logo");
 	logo.onclick = function clickToChoose() {
@@ -26,47 +29,68 @@ window.onload = function(){
 		};
 		console.log(computerChoice);
 		compare(userChoice, computerChoice);
+		document.getElementById("yourScore").innerHTML="Your score:";
+		document.getElementById("compScore").innerHTML="Computer score:";
+		document.getElementById("usercount").innerHTML=userCount;
+		document.getElementById("compcount").innerHTML=compCount;
 	};
-
 
 	var compare = function (choice1, choice2) {
 		if (choice1 === "rock")
 			if (choice2 === "paper"){
 				document.getElementById("notify").innerHTML="Paper covers rock";
+				document.getElementById("winlose").innerHTML="You lose!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				compCount += 1;
 			}
 			else if (choice2 === "scissors") {
 				document.getElementById("notify").innerHTML="Rock crushes scissors";
+				document.getElementById("winlose").innerHTML="You win!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				userCount += 1;
 			}
 			else if (choice2 === "lizard") {
 				document.getElementById("notify").innerHTML="Rock crushes lizard";
+				document.getElementById("winlose").innerHTML="You win!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				userCount += 1;
 			}
 			else if (choice2 === "spock") {
 				document.getElementById("notify").innerHTML="Spock vaporizes rock";
+				document.getElementById("winlose").innerHTML="You lose!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				compCount += 1;
+
 			}
 			else {
 				document.getElementById("notify").innerHTML="Tie!";
+				document.getElementById("winlose").innerHTML="You lose!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
 			}
 		else if (choice1 === "paper")
 			if (choice2 === "rock") {
 				document.getElementById("notify").innerHTML="Paper covers rock";
+				document.getElementById("winlose").innerHTML="You win!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				userCount += 1;
 			}
 			else if (choice2 === "scissors") {
 				document.getElementById("notify").innerHTML="Scissors cuts paper";
+				document.getElementById("winlose").innerHTML="You lose!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				compCount += 1;
 			}
 			else if (choice2 === "lizard") {
-				document.getElementById("line2").innerHTML="Click on the picture to play again!";
 				document.getElementById("notify").innerHTML="Lizard eats paper";
+				document.getElementById("winlose").innerHTML="You lose!";
+				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				compCount += 1;
 			}
 			else if (choice2 === "spock") {
 				document.getElementById("notify").innerHTML="Paper disproves Spock";
+				document.getElementById("winlose").innerHTML="You win!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				userCount += 1;
 			}
 			else {
 				document.getElementById("notify").innerHTML="Tie!";
@@ -75,19 +99,27 @@ window.onload = function(){
 		else if (choice1 ==="scissors" || choice1 === "sissors" || choice1 === "sisors")
 			if (choice2 === "paper") {
 				document.getElementById("notify").innerHTML="Scissors cuts paper";
+				document.getElementById("winlose").innerHTML="You win!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				userCount += 1;
 			}
 			else if (choice2 === "rock") {
 				document.getElementById("notify").innerHTML="Rock crushes scissors";
+				document.getElementById("winlose").innerHTML="You lose!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				compCount += 1;
 			}
 			else if (choice2 === "lizard") {
 				document.getElementById("notify").innerHTML="Scissors decapitate lizard";
+				document.getElementById("winlose").innerHTML="You win!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				userCount += 1;
 			}
 			else if (choice2 === "spock") {
 				document.getElementById("notify").innerHTML="Spock smashes scissors";
+				document.getElementById("winlose").innerHTML="You lose!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				compCount += 1;
 			}
 			else {
 				document.getElementById("notify").innerHTML="Tie!";
@@ -96,38 +128,54 @@ window.onload = function(){
 		else if (choice1 ==="lizard" || choice1 === "lisard" || choice1 === "lizzard")
 			if (choice2 === "paper") {
 				document.getElementById("notify").innerHTML="Lizard eats paper";
+				document.getElementById("winlose").innerHTML="You win!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				userCount += 1;
 			}
 			else if (choice2 === "rock") {
 				document.getElementById("notify").innerHTML="Rock crushes lizard";
+				document.getElementById("winlose").innerHTML="You lose!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				compCount += 1;
 			}
 			else if (choice2 === "scissors") {
 				document.getElementById("notify").innerHTML="Scissors decapitate lizard";
+				document.getElementById("winlose").innerHTML="You lose!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				compCount += 1;
 			}
 			else if (choice2 === "spock") {
 				document.getElementById("notify").innerHTML="Lizard poisons Spock";
+				document.getElementById("winlose").innerHTML="You win!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				userCount += 1;
 			}
 			else
 				{document.getElementById("notify").innerHTML="Tie!";}
 		else if (choice1 === "spock" || choice1 === "spoc" || choice1 === "spok")
 			if (choice2 === "paper") {
 				document.getElementById("notify").innerHTML="Paper disproves Spock";
+				document.getElementById("winlose").innerHTML="You lose!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				compCount += 1;
 			}
 			else if (choice2 === "rock") {
 				document.getElementById("notify").innerHTML="Spock vaporizes rock";
+				document.getElementById("winlose").innerHTML="You win!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				userCount += 1;
 			}
 			else if (choice2 === "lizard") {
 				document.getElementById("notify").innerHTML="Lizard poisons Spock";
+				document.getElementById("winlose").innerHTML="You lose!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				compCount += 1;
 			}
 			else if (choice2 === "scissors") {
 				document.getElementById("notify").innerHTML="Spock smashes scissors";
+				document.getElementById("winlose").innerHTML="You win!";
 				document.getElementById("line2").innerHTML="Click on the picture to play again!";
+				userCount += 1;
 			}
 			else {
 				document.getElementById("notify").innerHTML="Tie!";
